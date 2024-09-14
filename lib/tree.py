47980ded466e763fc97e6fcbe3c892342ecbdd26
WeddyNkatha265 <weddynkatha265@gmail.com>
2024-09-14 14:53:27 +0300

    Updates tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
-class Tree:
-  def __init__(self, root = None):
-    self.root = root
+from collections import deque
 
-  def get_element_by_id(self, id):
-    pass
+class Tree:
+    def __init__(self, root):
+        self.root = root
+
+    def get_element_by_id(self, element_id):
+        return self._bfs(self.root, element_id)
+
+    def _bfs(self, node, element_id):
+        queue = deque([node])
+        while queue:
+            current = queue.popleft()
+            if current['id'] == element_id:
+                return current
+            queue.extend(current['children'])
+        return None
